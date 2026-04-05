7f51186825961bf53f63e57139812c0e79b79b7b
Jagadeesh Pacha <jagadeesh1035.mail@gmail.com>
2026-04-05 11:44:52 +0530

    Updated task 2 and 3

--- python_basics_jagadeesh.py
+++ python_basics_jagadeesh.py
@@ -1,8 +1,24 @@
-"""Task1"""
+"""Task 1: Variables and Data Types"""
 name = "Jagadeesh"
 age = 35
 height = 5.9
 is_student = True
 
-print(f"Name: {name} Age: {age} Height: {height}, Is He Student?: {is_student}")
+print(name, age, height, is_student)
 
+"""Task 2: String Formatting"""
+print(f"Name: {name} | Age: {age} | Height: {height} | Student: {is_student}")
+
+
+"""Task 3: Arithmetic Operation"""
+
+"""Age in months
+Age in days (assume 365 days/year)
+The remainder when age is divided by 7
+Age raised to the power of 2"""
+
+
+print(f"Age in Months: {age*12} months")
+print(f"Age in Days: {age*365} days")
+print(f"The remainder when age is divided by 7: {age%7}")
+print(f"Age raised to the power of 2: {age**2}")
